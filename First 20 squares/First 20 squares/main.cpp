91c8f3e43000bc4368498e76ad0f3612335e4921
Nate Reynolds <natereynolds@Capitalist-Pig.local>
2018-09-29 15:00:32 -0700

    092918

---
+++ First 20 squares/First 20 squares/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  First 20 squares
+//
+//  Created by Nate Reynolds on 8/19/18.
+//  Copyright © 2018 Nate Reynolds. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    
+    for (int i = 0; i < 20; i++)
+    {
+        cout << pow(i, 2) << endl;
+    }
+    return 0;
+}
